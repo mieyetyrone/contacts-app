154ccc3f86901d39a56fa50858cdf95b3ca3b635
Mieye Tyrone Ombe <tdmye@yahoo.com>
2021-11-10 22:06:19 +0100

    UI integrated. Home page created.

---
+++ contacts/src/main/ui/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div className="container pt-3">
+      <h1>Contacts management</h1>
+      <Button variant="outline-secondary" style={{margin: "10px"}}>Add contact</Button>
+      <Button variant="outline-secondary" style={{margin: "10px"}}>View contacts</Button>
+    </div>
+    
+  );
+}
+
+export default App;
